d3f7ec4d7629ff7d5eaae4c3a52125e096790f34
Shivanshu Shrivastava <ssshrivastava2@gmail.com>
2023-02-01 20:28:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Billings from './components/Billings';
+import General from './components/General';
+import Integrations from './components/Integrations';
+import Plan from './components/Plan';
+import User from './components/User/User';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-9/12 p-6 m-6 bg-white border border-gray-200 rounded-lg shadow-lg'>
+      <h2 className='font-bold text-3xl pb-3'>Company Settings</h2>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}  />
+            <Route exact path='/plan' element={<Plan />} />
+            <Route exact path='/user' element={<User />} />
+            <Route exact path='/general' element={<General />} />
+            <Route exact path='/billings' element={<Billings />} />
+            <Route exact path='/integrations' element={<Integrations />} />
+
+        </Routes>
+      </Router>
     </div>
+
+
   );
 }
 
